b67a48baf7fe57968063c53188c14d6497275a61
RRoxxxsii <mishabur38@gmail.com>
2023-12-10 18:10:15 +0300

    reformated migration with linters

--- src/infrastructure/database/alembic/versions/1b758b2db1e3_initial.py
+++ src/infrastructure/database/alembic/versions/1b758b2db1e3_initial.py
@@ -1,21 +1,21 @@
 """initial
 
 Revision ID: 1b758b2db1e3
-Revises: 
+Revises:
 Create Date: 2023-12-10 14:42:26.629910
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
+from typing import Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '1b758b2db1e3'
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "1b758b2db1e3"
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
