99c2c98b2a3d801bd44f8863369e320b11742d63
RRoxxxsii <mishabur38@gmail.com>
2024-01-10 21:41:39 +0300

    Mocker API client in tests

--- tests/test_partners/test_routers.py
+++ tests/test_partners/test_routers.py
@@ -1,9 +1,15 @@
 from httpx import AsyncClient
+from pytest_mock import MockFixture
 from starlette import status
 
 
 class TestBecomePartnerExistsAccount:
-    async def test_become_partner(self, ac: AsyncClient, user, token):
+    async def test_become_partner(self, ac: AsyncClient, mocker: MockFixture, user, token):
+
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=True
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/upgrade-to-partner/",
             json={
@@ -18,13 +24,16 @@ class TestBecomePartnerExistsAccount:
             },
             headers={"Authorization": token},
         )
-
         assert response.status_code == status.HTTP_201_CREATED
 
-    async def test_become_partner_api_validation_failed(self, ac: AsyncClient, user, token):
+    async def test_become_partner_api_validation_failed(self, ac: AsyncClient, mocker: MockFixture, user, token):
         """
         Credentials validation through API failed.
         """
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=False
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/upgrade-to-partner/",
             json={
@@ -41,10 +50,15 @@ class TestBecomePartnerExistsAccount:
         )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    async def test_become_partner_credentials_exist(self, ac: AsyncClient, user, token, partner):
+    async def test_become_partner_credentials_exist(self, ac: AsyncClient, mocker: MockFixture, user, token, partner):
         """
         Credentials are not unique
         """
+
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=True
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/upgrade-to-partner/",
             json={
@@ -68,7 +82,12 @@ class TestBecomePartnerExistsAccount:
 
 
 class TestRegisterAsPartner:
-    async def test_register(self, ac: AsyncClient):
+    async def test_register(self, ac: AsyncClient, mocker: MockFixture):
+
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=True
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/register-as-partner/",
             json={
@@ -90,10 +109,14 @@ class TestRegisterAsPartner:
 
         assert response.status_code == status.HTTP_201_CREATED
 
-    async def test_become_partner_api_validation_failed(self, ac: AsyncClient, user, token):
+    async def test_become_partner_api_validation_failed(self, ac: AsyncClient, mocker: MockFixture, user, token):
         """
         Credentials validation through API failed.
         """
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=False
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/register-as-partner/",
             json={
@@ -115,10 +138,14 @@ class TestRegisterAsPartner:
         )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    async def test_become_partner_credentials_exist(self, ac: AsyncClient, user, token, partner):
+    async def test_become_partner_credentials_exist(self, ac: AsyncClient, mocker: MockFixture, user, token, partner):
         """
         Credentials are not unique
         """
+        mocker.patch(
+            "src.infrastructure.api_client.partners.implementation.Client.execute", return_value=True
+        )  # Mock Fake response
+
         response = await ac.post(
             "/partners/register-as-partner/",
             json={
